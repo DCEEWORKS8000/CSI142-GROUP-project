da49167d96319ff34de02f3185a52b6ae1b02def
Mphoentle-202501867 <setumomphoentletonic@gmail.com>
2026-04-22 17:10:11 +0200

    Update Contribution.java
    Adding the contribution class and editing the code

--- Contribution.java
+++ Contribution.java
@@ -8,14 +8,12 @@ public class Contribution {
     private double amount;
     private LocalDate date;
 
-    // Constructor
     public Contribution(Member member, double amount) {
         this.member = member;
         this.amount = amount;
         this.date = LocalDate.now(); // Automatically sets the date to today
     }
 
-    // Getters
     public Member getMember() {
         return member;
     }
@@ -28,10 +26,7 @@ public class Contribution {
         return date;
     }
 
-    // A display method to help with your group reports
     public void displayContribution() {
-        System.out.println("Member: " + member.getName() + 
-                           " | Amount: P" + amount + 
-                           " | Date: " + date);
+        System.out.println("Member: " + member.getName() + "  Amount: P" + amount + "  Date: " + date);
     }
   }
