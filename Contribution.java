9267fdcf157b9071887563eb89cb810ab2814933
Mphoentle-202501867 <setumomphoentletonic@gmail.com>
2026-04-22 17:04:31 +0200

    Update Contribution.java
    I was adding the contribution class

--- Contribution.java
+++ Contribution.java
@@ -1,1 +1,37 @@
-package ub.com.csi142.groupproject.model;
+package ub.com.csi142.groupproject model;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Contribution {
+    private Member member;
+    private double amount;
+    private LocalDate date;
+
+    // Constructor
+    public Contribution(Member member, double amount) {
+        this.member = member;
+        this.amount = amount;
+        this.date = LocalDate.now(); // Automatically sets the date to today
+    }
+
+    // Getters
+    public Member getMember() {
+        return member;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    // A display method to help with your group reports
+    public void displayContribution() {
+        System.out.println("Member: " + member.getName() + 
+                           " | Amount: P" + amount + 
+                           " | Date: " + date);
+    }
+  }
